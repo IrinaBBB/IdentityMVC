601d7333027afc528ba8288aabbe0b31467e5bbd
Irina <balkarovairina@gmail.com>
2021-06-13 19:42:05 +0200

    add: Register New User Method (Post Method)

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,11 +1,21 @@
 using System.Threading.Tasks;
 using IdentityManager.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IdentityManager.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
         [HttpGet]
         public IActionResult Register()
         {
@@ -20,11 +30,25 @@ namespace IdentityManager.Controllers
             if (ModelState.IsValid)
             {
                 var user = new ApplicationUser { UserName = model.Email, Email = model.Email, Name = model.Name };
+                var result = await _userManager.CreateAsync(user, model.Password);
 
-                
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                AddErrors(result);
             }
-            RegisterViewModel registerViewModel = new RegisterViewModel();
-            return View(registerViewModel);
+
+            return View(model);
+        }
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
         }
     }
 }
