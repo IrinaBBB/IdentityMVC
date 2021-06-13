a8fdd2b9505267ebb2f18cf3c654f4e0565fb6f6
Irina <balkarovairina@gmail.com>
2021-06-13 16:28:02 +0200

    add: Register and Login ViewModels

---
+++ Models/RegisterViewModel.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace IdentityManager.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name="Password")]
+        [DataType(DataType.Password)]
+        [StringLength(100, ErrorMessage ="The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
