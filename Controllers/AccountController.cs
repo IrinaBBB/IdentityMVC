3e67508edc012ec854835f004a9a3ca481a28c47
Irina <balkarovairina@gmail.com>
2021-06-13 19:02:44 +0200

    fix: some fixes in several files

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -12,5 +12,19 @@ namespace IdentityManager.Controllers
             RegisterViewModel registerViewModel = new RegisterViewModel();
             return View(registerViewModel);
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, Name = model.Name };
+
+                
+            }
+            RegisterViewModel registerViewModel = new RegisterViewModel();
+            return View(registerViewModel);
+        }
     }
 }
--- Models/RegisterViewModel.cs
+++ Models/RegisterViewModel.cs
@@ -10,7 +10,7 @@ namespace IdentityManager.Models
         public string Email { get; set; }
 
         [Required]
-        [Display(Name="Password")]
+        [Display(Name = "Password")]
         [DataType(DataType.Password)]
         [StringLength(100, ErrorMessage ="The {0} must be at least {2} characters long.", MinimumLength = 6)]
         public string Password { get; set; }
